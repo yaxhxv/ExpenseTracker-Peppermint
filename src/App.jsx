b2870dafd3d1c9483c568ed1ef751b644183bd4c
yaxh.xv <yashnawgiri45@gmail.com>
2024-09-27 13:03:23 +0530

    Created a list of Folders for Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Dashboard from "./components/Dashboard/Dashboard.jsx"
 
-import './App.css'
 
 function App() {
-
-
   return (
-    <>
-    <h1 className="text-3xl font-bold underline text-red-500 bg-cyan-600 flex justify-center text-center py-3 px-3 h-6 pt-3">
-      Hello world!
-    </h1>
-    </>
-  )
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
 }
 
-export default App
+export default App;
